2b45c0603f1e30ec2b7147837930690438b6bfad
Sam Page <samnpage96@gmail.com>
2024-04-15 16:59:23 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Objective: Build an app that is functionally similar to https://roman-numeral-converter.freecodecamp.rocks
+
+// User Stories:
+
+// You should have an input element with an id of "number"
+// You should have a button element with an id of "convert-btn"
+// You should have a div element with an id of output
+// When you click on the #convert-btn element without entering a value into the #number element, the #output element should contain the text "Please enter a valid number"
+// When the #number element contains the number -1 and the #convert-btn element is clicked, the #output element should contain the text "Please enter a number greater than or equal to 1"
+// When the #number element contains the number 4000 or greater and the #convert-btn element is clicked, the #output element should contain the text "Please enter a number less than or equal to 3999"
+// When the #number element contains the number 9 and the #convert-btn element is clicked, the #output element should contain the text "IX"
+// When the #number element contains the number 16 and the #convert-btn element is clicked, the #output element should contain the text "XVI"
+// When the #number element contains the number 649 and the #convert-btn element is clicked, the #output element should contain the text "DCXLIX"
+// When the #number element contains the number 1023 and the #convert-btn element is clicked, the #output element should contain the text "MXXIII"
+// When the #number element contains the number 3999 and the #convert-btn element is clicked, the #output element should contain the text "MMMCMXCIX"
+// Fulfill the user stories and pass all the tests below to complete this project. Give it your own personal style. Happy Coding!
